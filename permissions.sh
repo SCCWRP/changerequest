386b1c663faeba5fa16dcc80e49dae211198526e
Robert Butler <r7butler@yahoo.com>
2022-08-12 02:13:03 -0700

    chown on certain folders needed to be made recursive

--- permissions.sh
+++ permissions.sh
@@ -9,5 +9,5 @@ fi
 DIRECTORY=$(cd `dirname $0` && pwd)
 chown -R $editor.$editor $DIRECTORY;
 chown -R www-data.$editor $DIRECTORY/files $DIRECTORY/export $DIRECTORY/proj/custom $DIRECTORY/proj/static $DIRECTORY/proj/config;
-chmod 775 $DIRECTORY/files $DIRECTORY/export $DIRECTORY/proj/custom $DIRECTORY/proj/static;
-chmod 755 $DIRECTORY/proj/config;
+chmod -R 775 $DIRECTORY/files $DIRECTORY/export $DIRECTORY/proj/custom $DIRECTORY/proj/static;
+chmod -R 575 $DIRECTORY/proj/config;

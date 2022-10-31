328fdccbb53a6f380dd715b7b93389a20e835b75
Robert Butler <r7butler@yahoo.com>
2022-10-31 10:57:29 -0700

    if no modifications made / no modified records - dont attempt to run checks and comparison on the data

--- proj/static/save.js
+++ proj/static/save.js
@@ -1,57 +1,60 @@
 import { addTips } from "./tooltip.js";
 // Converts the html table displayed in the 'edit_submission' page to JSON that is formatted for to work like a pandas dataframe
 export const saveChanges = function() {
-    let table = document.querySelector('div.records-display-inner-container table');
-    let rows = table.querySelectorAll('tr');
+    if (document.querySelector('.records-display-inner-container table tbody').children.length > 0){
+        let table = document.querySelector('div.records-display-inner-container table');
+        let rows = table.querySelectorAll('tr');
 
-    // show the loader gif
-    // script root is a global, defined in script tags in the head of the HTML document
-    document.querySelector(".records-display-inner-container").innerHTML = `<img src="/${$SCRIPT_ROOT}/static/loading.gif">`;
+        // show the loader gif
+        // script root is a global, defined in script tags in the head of the HTML document
+        document.querySelector(".records-display-inner-container").innerHTML = `<img src="/${$SCRIPT_ROOT}/static/loading.gif">`;
 
-    let tableJSON = Array.from(rows).slice(1).map(row => {
-        let record = new Object();
-        Array.from(row.querySelectorAll('td')).forEach(
-            cell => {
-                let colnameArray = Array.from(cell.classList).filter(cl => cl.includes('colname-'));
+        let tableJSON = Array.from(rows).slice(1).map(row => {
+            let record = new Object();
+            Array.from(row.querySelectorAll('td')).forEach(
+                cell => {
+                    let colnameArray = Array.from(cell.classList).filter(cl => cl.includes('colname-'));
 
-                // The table created by the app on the backend should be following a certain class naming convention
-                console.assert(
-                    colnameArray.length === 1, 
-                    "table cell class naming convention not followed - there should be exactly one class that says colname-<colname> so the table can be converted to a json more efficiently"
-                );
+                    // The table created by the app on the backend should be following a certain class naming convention
+                    console.assert(
+                        colnameArray.length === 1, 
+                        "table cell class naming convention not followed - there should be exactly one class that says colname-<colname> so the table can be converted to a json more efficiently"
+                    );
 
-                let colname = colnameArray[0].replace('colname-','');
-                
-                record[colname] = cell.innerText;
-                return ;
-            }
-        )
-        return record;
-    })
+                    let colname = colnameArray[0].replace('colname-','');
+                    
+                    record[colname] = cell.innerText;
+                    return ;
+                }
+            )
+            return record;
+        })
 
-    tableJSON = JSON.stringify(tableJSON);
-
-    console.log(tableJSON);
-
-     // Send the edited records to the server
-    fetch(`/${$SCRIPT_ROOT}/compare`, {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: tableJSON
-    })
-    .then(resp => {
-        //console.log(resp.json());
-        return resp.json()
-    })
-    .then(data => {
-        console.log(data);
-        document.querySelector(".records-display-inner-container").innerHTML = data.tbl;
-        formatDataTable(data);
-        addTips();
-        return data;
-    })
-    .catch(err => {
-        console.log(err);
-    })
-
+        tableJSON = JSON.stringify(tableJSON);
+        console.log("tableJSON")
+        console.log(tableJSON);
+        
+        // Send the edited records to the server
+        fetch(`/${$SCRIPT_ROOT}/compare`, {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: tableJSON
+        })
+        .then(resp => {
+            //console.log(resp.json());
+            return resp.json()
+        })
+        .then(data => {
+            console.log(data);
+            document.querySelector(".records-display-inner-container").innerHTML = data.tbl;
+            formatDataTable(data);
+            addTips();
+            return data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    } else {
+        alert("No changes were made")
+    }
 }
